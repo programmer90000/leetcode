4d9df1a479262ffd76fefcdb8758989260dc7dbd
Abdul Rehman <abdulr568programming@outlook.com>
2024-02-15 10:31:41 +0000

    Check if number is positive or negative

--- challenge-8/script.js
+++ challenge-8/script.js
@@ -1,5 +1,16 @@
 var myAtoi = function (s) {
     s = s.trim();
+
+    let sign;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "-") {
+            sign = "negative";
+            break;
+        } else {
+            sign = "positive";
+        }
+    }
+
     return s;
 };
 
