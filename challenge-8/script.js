9435f365c54f63cdbb94fb4c6e15a86b85bc1e1b
Abdul Rehman <abdulr568programming@outlook.com>
2024-02-17 11:54:55 +0000

    Remove decimal point

--- challenge-8/script.js
+++ challenge-8/script.js
@@ -3,6 +3,12 @@ var myAtoi = function (s) {
 
     s = s.trim();
 
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === ".") {
+            s = s.slice(0, i);
+        }
+    }
+
     while (s[0] === "0") {
         s = s.substring(1)
     }
@@ -38,3 +44,4 @@ console.log(myAtoi("   -42"));
 console.log(myAtoi("4193 with words"));
 console.log(myAtoi("-2147483648"));
 console.log(myAtoi("words and 987"));
+console.log(myAtoi("3.14159"));
