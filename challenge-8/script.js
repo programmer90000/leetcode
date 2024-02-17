1adac1eee01faa3b4a3538ad26caee4bfa2e2aa1
Abdul Rehman <abdulr568programming@outlook.com>
2024-02-17 12:50:27 +0000

    Challenge 8

--- challenge-8/script.js
+++ challenge-8/script.js
@@ -1,50 +1,13 @@
-var myAtoi = function (s) {
-    let negative;
+var myAtoi = function (str) {
+    const converted = parseInt(str);
 
-    s = s.trim();
-
-    // Remove any characters after the dot
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === ".") {
-            s = s.slice(0, i);
-        }
+    if (converted > 2147483647) {
+        return 2147483647;
+    } else if (converted < -2147483648) {
+        return -2147483648;
     }
 
-    // Remove any 0's at the beginging of the string
-    while (s[0] === "0") {
-        s = s.substring(1)
-    }
-
-    // Check of there is a -
-    if (s[0] === "-") {
-        negative = true;
-    }
-
-    // Check if the number is valid
-    if ((!Number(s[0])) && (s[0] !== "-") && (s[0] !== "+")) {
-        return 0;
-    }
-
-    // Turn the string into a number
-    if (!parseInt(s)) { return 0; }
-    s = s.replace(/\D/g, "");
-    s = parseInt(s);
-    s = Number(s);
-
-    // If the number is negative, add a - sign
-    if (negative) {
-        s = s * -1;
-    }
-
-    // If the number is too big or too small, make it the correct size
-    if (s > Math.pow(2, 31) - 1) {
-        s = Math.pow(2, 31) - 1;
-    }
-    if (s < Math.pow(-2, 31)) {
-        s = Math.pow(-2, 31);
-    }
-
-    return s;
+    return isNaN(converted) ? 0 : converted;
 };
 
 console.log(myAtoi("-21474836479999999"));
