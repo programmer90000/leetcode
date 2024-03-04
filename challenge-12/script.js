27517c611e7f4241abe7cf0007315f1cfd99f7da
Abdul Rehman <abdulr568programming@outlook.com>
2024-03-04 10:38:33 +0000

    Reverse Array

--- challenge-12/script.js
+++ challenge-12/script.js
@@ -1,10 +1,15 @@
 var intToRoman = function (num) {
     const numberString = num.toString();
-    let i = 1;
+    const newNumberArray = [];
     const numberArray = [...numberString].map((digit => {
         return parseInt(digit);
     }));
-    return numberArray;
+
+    for (let i = numberArray.length - 1; i >= 0; i--) {
+        newNumberArray.push(numberArray[i]);
+    }
+
+    return newNumberArray;
 };
 
 console.log(intToRoman(123456789));
