6635a3b8df7cbc72d8b5dfb6e88a39bf909c0167
Abdul Rehman <abdulr568programming@outlook.com>
2024-03-04 17:29:02 +0000

    Challenge 12

--- challenge-12/script.js
+++ challenge-12/script.js
@@ -1,19 +1,19 @@
 var intToRoman = function (num) {
-    const numberString = num.toString();
-    const newNumberArray = [];
-    const numberArray = [...numberString].map((digit => {
-        return parseInt(digit);
-    }));
+    const symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
-    let multiplication = 1;
+    let result = "";
 
-    for (let i = numberArray.length - 1; i >= 0; i--) {
-        let nums = numberArray[i] * multiplication;
-        multiplication = multiplication * 10;
-        newNumberArray.push(nums);
+    for (let i = 0; i < values.length; i++) {
+        while (num >= values[i]) {
+            result += symbols[i];
+            num -= values[i];
+        }
     }
 
-    return newNumberArray;
+    return result;
 };
 
-console.log(intToRoman(123456789));
+console.log(intToRoman(3));
+console.log(intToRoman(58));
+console.log(intToRoman(1994));
