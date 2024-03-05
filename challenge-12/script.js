9d9436ce40fba052b23c71e7e1acbc2312e89564
Abdul Rehman <abdulr568programming@outlook.com>
2024-03-05 10:04:38 +0000

    Challenge 12

--- challenge-12/script.js
+++ challenge-12/script.js
@@ -1,9 +1,11 @@
 var intToRoman = function (num) {
+    // Create a symbols Array and a values Array. The values in the same index of both values match
     const symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
     const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
     let result = "";
 
+    // For every item inside the values Array, check if num is bigger than it. 
     for (let i = 0; i < values.length; i++) {
         while (num >= values[i]) {
             result += symbols[i];
