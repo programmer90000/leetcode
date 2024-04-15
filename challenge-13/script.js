6a3e2444b9bfbfcbf0bcb498a09248b52e7db12b
Abdul Rehman <abdulr568programming@outlook.com>
2024-04-15 16:31:19 +0100

    Updated variable names

--- challenge-13/script.js
+++ challenge-13/script.js
@@ -1,13 +1,13 @@
-var romanToInt = function (num) {
+var romanToInt = function (romanNumeral) {
     // Create a symbols Array and a values Array. The values in the same index of both values match
     const symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
     const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
     let result = "";
 
-    // For every item inside the values Array, check if num is bigger than it. 
-    for (let i = 0; i < num.length; i++) {
-        console.log(num[i]);
+    // For every item inside the values Array, check if romanNumeral is bigger than it. 
+    for (let i = 0; i < romanNumeral.length; i++) {
+        console.log(romanNumeral[i]);
     }
 
     return result;
