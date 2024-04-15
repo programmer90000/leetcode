ed7d399d30184ff2db0248ca7dc4deb9f3bbee9f
Abdul Rehman <abdulr568programming@outlook.com>
2024-04-15 16:14:11 +0100

    Console log each letter

---
+++ challenge-13/script.js
@@ -0,0 +1,18 @@
+var romanToInt = function (num) {
+    // Create a symbols Array and a values Array. The values in the same index of both values match
+    const symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+
+    let result = "";
+
+    // For every item inside the values Array, check if num is bigger than it. 
+    for (let i = 0; i < num.length; i++) {
+        console.log(num[i]);
+    }
+
+    return result;
+};
+
+console.log(romanToInt("III"));
+console.log(romanToInt("LVIII"));
+console.log(romanToInt("MCMXCIV"));
