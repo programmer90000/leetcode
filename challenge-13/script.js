0ad927e683486b288f116b2b25c028f90b2bd4c4
Abdul Rehman <abdulr568programming@outlook.com>
2024-04-15 17:34:08 +0100

    Change Arrays to Object

--- challenge-13/script.js
+++ challenge-13/script.js
@@ -1,7 +1,19 @@
 var romanToInt = function (romanNumeral) {
-    // Create a symbols Array and a values Array. The values in the same index of both values match
-    const symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
-    const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const romanToIntMap = {
+        'I': 1,
+        'IV': 4,
+        'V': 5,
+        'IX': 9,
+        'X': 10,
+        'XL': 40,
+        'L': 50,
+        'XC': 90,
+        'C': 100,
+        'CD': 400,
+        'D': 500,
+        'CM': 900,
+        'M': 1000
+    };
 
     let result = 0;
 
